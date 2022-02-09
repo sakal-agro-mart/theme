d47aa104288d093fafe6f7a97f80fd273b869e34
AabhasDhaubanja <aabhas41@gmail.com>
2022-02-09 18:13:09 +0545

    fixed the nav

--- js/navigation.js
+++ js/navigation.js
@@ -1,16 +1,28 @@
+const hamburger = document.getElementById("navbar-hamburger");
 
-const hamburger = document.getElementById('navbar-hamburger');
+const menuLinks = document.getElementById("menu-links-wrapper");
 
-const menuLinks =document.getElementById('menu-links-wrapper')
+const nav = document.getElementById("site-navigation");
 
-hamburger.addEventListener('click', (e) => {
-	if(menuLinks.style.visibility == 'visible') {
-		menuLinks.style.visibility = 'hidden';
-		menuLinks.style.opacity = 0;
+window.addEventListener("click", (e) => {
+  // if the click is in the elements inside of nav then
+  if (nav.contains(e.target)) {
+    // if not clicked in the hamburger just return i.e. terminate
+    if (!hamburger.contains(e.target)) return;
 
-	} else {
-		menuLinks.style.visibility = 'visible';
-		menuLinks.style.opacity = 1;
-	}
-	
+    // toggle
+    if (menuLinks.style.visibility == "visible") {
+      menuLinks.style.visibility = "hidden";
+      menuLinks.style.opacity = 0;
+    } else {
+      menuLinks.style.visibility = "visible";
+      menuLinks.style.opacity = 1;
+    }
+  } else {
+    // if the click is outside the nav then hide it :D
+    if (menuLinks.style.visibility == "visible") {
+      menuLinks.style.visibility = "hidden";
+      menuLinks.style.opacity = 0;
+    }
+  }
 });
